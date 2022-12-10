44801b80168b381ff0e7f0056f7bf7b079e6353d
Andres Crosa <andresmcrosa@gmail.com>
2022-12-10 20:42:18 -0300

    Cambios

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/', (req, res)=> {
     console.log('peticion recibida')
 
-    res.send('<h1>Hola Mundo!!</h1>')
+    res.send('<h1>Mi primer Hola Mundo!!</h1>')
 })
 
 const PORT = process.env.PORT || 3000
