7e5bb745c72d6c07b4fa733854d6e07e8fe85c27
Andres Crosa <andresmcrosa@gmail.com>
2022-12-10 23:20:23 -0300

    Conexion con MongoDB + HTML CSS y JS

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path =require('path')
 require('dotenv').config()
 
-const express = require('express')
+mongoose.set('strictQuery', false);
 
 const app = express()
 
-app.get('/', (req, res)=> {
-    console.log('peticion recibida')
-
-    res.send('<h1>Mi primer Hola Mundo!!</h1>')
+mongoose
+.connect(`mongodb+srv://Acrosa86:${process.env.MONGO_DB_PASS}@development.vkwbcei.mongodb.net/stock-app?retryWrites=true&w=majority`
+)
+.then( (result) => {
+    
+    app.listen(PORT, () => {
+        console.log (`Servidor escuchando en el puerto ${PORT}`)
+    })
+    console.log ('Conexion exitosa a la BBDD')
 })
+.catch((err) => console.log(err))
+
+const productSchema = mongoose.Schema({
+    name: {type: String, require: true},
+    price: Number,
+},
+{timestamps: true}
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+
+app.use(express.json())
+
+
+app.post('/api/v1/products', (req, res)=> {
+   
+    const newProduct = new Product(req.body)
+
+    newProduct
+    .save()
+    .then( result => {
+
+    res.status(201).json({ ok: true })
+    })
+    .catch((err) => console.log(err)) 
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Empezamos asi, pero como en diferentes sistemas operativos, 
+// la separacion de las carpetas del path difiere / o \ utilizamos el path.join de arriba.
+
+//app.get('/', (req, res)=> {
+//   console.log('Peticion recibida')
+//
+//    res.status(200).sendFile('index.html',{root: __dirname})
+//})
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log (`Servidor escuchando en el puerto ${PORT}`)
-})
